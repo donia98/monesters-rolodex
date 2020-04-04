fb7079e7fcedc40bb184d36e22907ece273f21d1
donia98 <nadooshah98@gmail.com>
2020-04-05 00:16:03 +0300

    Updates

---
+++ precache-manifest.f34bf554999be85028c00cd9c564653b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadf5df61f142e74dd90b41c0d1f7178",
+    "url": "/monesters-rolodex/index.html"
+  },
+  {
+    "revision": "9dec645d72c2791d29f5",
+    "url": "/monesters-rolodex/static/css/main.4be08968.chunk.css"
+  },
+  {
+    "revision": "1dd4225db137bf31b171",
+    "url": "/monesters-rolodex/static/js/2.c3369c20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monesters-rolodex/static/js/2.c3369c20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9dec645d72c2791d29f5",
+    "url": "/monesters-rolodex/static/js/main.89012eb4.chunk.js"
+  },
+  {
+    "revision": "682d8a2f90ae1c80c80e",
+    "url": "/monesters-rolodex/static/js/runtime-main.ea6e5ca1.js"
+  }
+]);
